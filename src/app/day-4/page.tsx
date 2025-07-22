81c79b60ec8307a2d8e44a73b85d8133a743e937
Girish Sawant <girishsawant999.gs@gmail.com>
2025-07-22 14:39:47 +0530

    feat: added flip images gallery

--- src/app/day-4/page.tsx
+++ src/app/day-4/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import BackButton from "@/components/BackButton";
+import { MODELS_IMAGES } from "@/constants/Models";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Flip from "gsap/Flip";
@@ -10,32 +11,16 @@ import {
   RotateCcw,
   Triangle,
 } from "lucide-react";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 gsap.registerPlugin(Flip);
 
-const FlipTutorial = () => {
-  const container = useRef(null);
-  const bugRef = useRef<SVGSVGElement | null>(null);
+const KillBugsGame = () => {
   const [bugIndexes, setBugIndexes] = useState<number[]>([]);
   const [bugSpeed, setBugSpeed] = useState(2500);
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(3);
-
-  useGSAP(
-    () => {
-      gsap.to("#scroll-down", {
-        y: 30,
-        ease: "power2.in",
-        duration: 0.5,
-        repeat: -1,
-        yoyo: true,
-      });
-    },
-    {
-      scope: container,
-    }
-  );
 
   useEffect(() => {
     if (lives === 0) return setBugIndexes([]);
@@ -67,7 +52,6 @@ const FlipTutorial = () => {
     const targetIndex = Array.from(
       document.querySelectorAll(".targets")
     ).indexOf(target);
-    const bugEl = bugRef.current;
     const isHit = bugIndexes.includes(targetIndex);
 
     gsap.to(movingObject, {
@@ -128,6 +112,103 @@ const FlipTutorial = () => {
   };
 
   return (
+    <div className="grid min-h-dvh snap-start place-items-center relative primary-section w-full">
+      <div className="w-full max-w-xl flex justify-between items-center mb-4">
+        <div className="flex items-center gap-2">
+          {Array.from({ length: lives }).map((_, i) => (
+            <Heart
+              key={i}
+              className="text-red-500"
+              size={20}
+              fill="currentColor"
+            />
+          ))}
+        </div>
+        <div className="text-sm font-semibold">Score: {score}</div>
+        <button
+          onClick={handleReset}
+          className="cursor-pointer flex items-center gap-1 px-2 py-1 text-sm border rounded bg-background hover:bg-background/50 border-foreground"
+        >
+          {lives === 0 ? (
+            <>
+              <Triangle className="rotate-90" size={14} />
+              Start
+            </>
+          ) : (
+            <>
+              <RotateCcw size={14} />
+              Reset
+            </>
+          )}
+        </button>
+      </div>
+
+      <div className="grid grid-cols-8 grid-rows-8 place-items-stretch w-full max-w-xl">
+        {Array.from({ length: 64 }).map((_, i) => (
+          <div
+            onClick={switchPosition}
+            className="targets cursor-grab  relative grid place-items-center bg-foreground/10 border border-solid border-foreground/40 aspect-square"
+            key={i}
+          >
+            {i === 28 && (
+              <div
+                id="moving-object"
+                className="w-2/3 aspect-square bg-foreground rounded-full"
+              />
+            )}
+
+            {bugIndexes.map((idx, j) => {
+              if (idx !== i) return null;
+              return (
+                <BugIcon
+                  data-bug={j}
+                  key={j}
+                  className="absolute w-1/2 aspect-square text-red-600 dark:text-rose-400 rounded-full"
+                />
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const FlipTutorial = () => {
+  const bigLayout = { col: 1, row: 1, colSpan: 6, rowSpan: 6 };
+  const smallLayout = { col: "", row: "", colSpan: 3, rowSpan: 3 };
+
+  const container = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useGSAP(
+    () => {
+      gsap.to("#scroll-down", {
+        y: 30,
+        ease: "power2.in",
+        duration: 0.5,
+        repeat: -1,
+        yoyo: true,
+      });
+    },
+    {
+      scope: container,
+    }
+  );
+
+  const switchToBigGrid = (idx: number) => () => {
+    const state = Flip.getState("[data-grid]");
+    setActiveIndex(idx);
+    requestAnimationFrame(() => {
+      Flip.from(state, {
+        duration: 0.5,
+        ease: "power2.inOut",
+        fade: true,
+      });
+    });
+  };
+
+  return (
     <section
       ref={container}
       className="bg-background text-foreground min-h-dvh p-5 md:p-10 flex flex-col place-items-center scroll-smooth snap-y snap-mandatory"
@@ -144,64 +225,46 @@ const FlipTutorial = () => {
           id="scroll-down"
           className="absolute bottom-10 flex flex-col items-center gap-1 justify-center"
         >
-          <span className="text-lg font-normal">Scroll down</span>
+          <span className="text-lg font-normal">Scroll down & click</span>
           <ChevronsDown size={18} />
         </div>
       </div>
-      <div className="grid min-h-dvh snap-start place-items-center relative primary-section w-full">
-        <div className="w-full max-w-xl flex justify-between items-center mb-4">
-          <div className="flex items-center gap-2">
-            {Array.from({ length: lives }).map((_, i) => (
-              <Heart
-                key={i}
-                className="text-red-500"
-                size={20}
-                fill="currentColor"
-              />
-            ))}
-          </div>
-          <div className="text-sm font-semibold">Score: {score}</div>
-          <button
-            onClick={handleReset}
-            className="cursor-pointer flex items-center gap-1 px-2 py-1 text-sm border rounded bg-background hover:bg-background/50 border-foreground"
-          >
-            {lives === 0 ? (
-              <>
-                <Triangle className="rotate-90" size={14} />
-                Start
-              </>
-            ) : (
-              <>
-                <RotateCcw size={14} />
-                Reset
-              </>
-            )}
-          </button>
-        </div>
 
-        <div className="grid grid-cols-8 grid-rows-8 place-items-stretch w-full max-w-xl">
-          {Array.from({ length: 64 }).map((_, i) => (
-            <div
-              onClick={switchPosition}
-              className="targets cursor-grab  relative grid place-items-center bg-foreground/10 border border-solid border-foreground/40 aspect-square"
-              key={i}
-            >
-              {i === 28 && (
-                <div
-                  id="moving-object"
-                  className="w-2/3 aspect-square bg-foreground rounded-full"
+      <div className="min-h-dvh  w-full grid place-items-center ">
+        <div className="grid grid-cols-9 grid-rows-9 gap-1 md:gap-4 max-w-2xl w-full aspect-square bg-white border-4 md:border-[16px] border-white">
+          {MODELS_IMAGES.map((url, index) => {
+            const {
+              col = "",
+              row = "",
+              colSpan,
+              rowSpan,
+            } = index === activeIndex ? bigLayout : smallLayout;
+
+            return (
+              <figure
+                key={index}
+                data-grid={index}
+                onClick={switchToBigGrid(index)}
+                className={`col-span-${colSpan} row-span-${rowSpan} col-start-${col} row-start-${row} ${
+                  index !== 0 ? "cursor-zoom-in" : ""
+                }`}
+              >
+                <Image
+                  src={url}
+                  className="h-full w-full object-cover object-top"
+                  alt={`Profile ${index}`}
+                  width={1200}
+                  height={1200}
+                  quality={100}
+                  loading="eager"
                 />
-              )}
-              {bugIndexes.includes(i) && (
-                <BugIcon
-                  ref={i === bugIndexes[0] ? bugRef : null}
-                  className="absolute w-1/2 aspect-square text-red-600 dark:text-rose-400 rounded-full"
-                />
-              )}
-            </div>
-          ))}
+              </figure>
+            );
+          })}
         </div>
       </div>
+
+      <KillBugsGame />
     </section>
   );
 };
