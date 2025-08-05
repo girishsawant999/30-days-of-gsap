8f20e8a2417c78f65e795f3376a078f598bde2ee
Girish Sawant <girishsawant999.gs@gmail.com>
2025-08-05 18:29:55 +0530

    ✨ feat: Add LANDSCAPES_IMAGES constant
    - Adds a new file src/constants/Landscapes.tsx.
    - Defines a constant `LANDSCAPES_IMAGES` which is an array of objects.
    - Each object contains `src` and `thumbnail` properties representing image URLs.

---
+++ src/constants/Landscapes.tsx
@@ -0,0 +1,30 @@
+export const LANDSCAPES_IMAGES = [
+  {
+    src: "https://images.unsplash.com/photo-1646654922049-3c929366b298",
+    thumbnail: "https://images.unsplash.com/photo-1584308972272-9e4e7685e80f",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1453872302360-eed3c5f8ff66",
+    thumbnail: "https://images.unsplash.com/photo-1586299485759-f62264d6b63f",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1533760881669-80db4d7b4c15",
+    thumbnail: "https://images.unsplash.com/photo-1485893086445-ed75865251e0",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1506918357967-b358ad447c56",
+    thumbnail: "https://images.unsplash.com/photo-1545194827-db87df1649d9",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1460627390041-532a28402358",
+    thumbnail: "https://images.unsplash.com/photo-1669501084883-85849ad2d40f",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1515404929826-76fff9fef6fe",
+    thumbnail: "https://images.unsplash.com/photo-1572803124538-b039f453f00b",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1462400362591-9ca55235346a",
+    thumbnail: "https://images.unsplash.com/photo-1629580627189-4ab632f73dda",
+  },
+];
