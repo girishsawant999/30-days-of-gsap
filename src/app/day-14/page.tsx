b74ecedc0a4530ede3ba9fbd3e8a6f4f4b38dcaf
Girish Sawant <girishsawant999.gs@gmail.com>
2025-08-11 20:46:46 +0530

    ✨ feat: Add day 14 card stack animation and update active days
    - Added a new page for day 14 with a card stack animation.
    - Updated the number of active days from 13 to 14 on the homepage.

---
+++ src/app/day-14/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import BackButton from "@/components/BackButton";
+import ScrollToExplore from "@/components/ScrollToExplore";
+import { LANDSCAPES_IMAGES } from "@/constants/Landscapes";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Image from "next/image";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const CardStacks = () => {
+  const scaleMap = gsap.utils.mapRange(0, 1, 2, 1);
+  useGSAP(() => {
+    const cards = gsap.utils.toArray(".card");
+
+    cards.forEach((card, index) => {
+      ScrollTrigger.create({
+        trigger: card as HTMLElement,
+        start: "start, bottom",
+        end: "start, top",
+        scrub: true,
+        onUpdate: ({ progress, ...args }) => {
+          gsap.set((card as HTMLElement).querySelector("img"), {
+            scale: scaleMap(progress),
+          });
+        },
+        onLeave: () => {
+          gsap.to(card as HTMLElement, {
+            scale: 1 - (cards.length - index) * 0.02,
+          });
+        },
+      });
+    });
+  });
+
+  return (
+    <section className="pt-[50vh] pb-[100vh] bg-background text-foreground">
+      <div className="fixed flex items-center gap-3 top-5 md:top-10 left-5 md:left-10 z-10">
+        <BackButton />
+      </div>
+      <ScrollToExplore />
+      {LANDSCAPES_IMAGES.map(({ src, thumbnail }, index) => (
+        <div
+          className="sticky top-0 h-screen grid place-items-center card origin-top"
+          key={index}
+        >
+          <div
+            className="relative p-5 rounded-lg bg-foreground shadow-lg"
+            style={{
+              top: `calc(-10% + ${index * 25}px)`,
+            }}
+          >
+            <div className="overflow-hidden rounded-md">
+              <Image
+                src={src}
+                width={800}
+                height={500}
+                alt={`Landscape ${index + 1}`}
+                className="object-cover w-[30vw]"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default CardStacks;
